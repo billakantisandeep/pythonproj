ac3e1a89e5974f377c4c10aa757fe3cfb3884d2b
Sandeep Billakanti <billakantisandeep@gmail.com>
2019-05-06 10:44:39 +0530

    Removed code and added new

--- intro.py
+++ intro.py
@@ -1,8 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-name = input("your Name")
-print("Hello", name)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
